bab139d51e7ea0b6f4cde0377d511712d345d17b
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2025-03-19 21:12:38 +0100

    commited

--- helloWorld/src/main/java/com/example/helloWorld/HelloController.java
+++ helloWorld/src/main/java/com/example/helloWorld/HelloController.java
@@ -1,4 +1,15 @@
 package com.example.helloWorld;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
 public class HelloController {
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!!!";
+    }
+    // after input "mvn spring-boot:run" on the terminal open the "http://localhost:8080/"
 }
